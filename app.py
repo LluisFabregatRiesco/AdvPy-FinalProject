aa3f57a79faaa0adbe4fb9f9223695e704740b53
Cavan P <53267753+Cavan-P@users.noreply.github.com>
2023-11-22 10:54:58 -0700

    Basic Flask web app with a couple different pages/templates

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/soccer')
+def soccer():
+    return render_template('soccer.html')
+
+@app.route('/baseball')
+def baseball():
+    return render_template('baseball.html')
