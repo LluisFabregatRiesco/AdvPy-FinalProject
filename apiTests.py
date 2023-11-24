cb90a3afddc15b5313c57a59506691de78012fba
LluisFabregatRiesco <luigi.fabregat@gmail.com>
2023-11-24 22:13:55 +0000

    Fixed flake8 problems

--- apiTests.py
+++ apiTests.py
@@ -1,12 +1,16 @@
 import requests
 
-url = "https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeams"
+url = "https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com"
+#/getMLBTeams"
 
-querystring = {"teamStats":"false","topPerformers":"false"}
+
+querystring = {"teamStats": "false", "topPerformers": "false"}
 
 headers = {
-    "X-RapidAPI-Key": "My API Key probably not important to hide this but oh well",
-    "X-RapidAPI-Host": "tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com"
+    "X-RapidAPI-Key": """My API Key probably not important to hide
+                         this but oh well""",
+    "X-RapidAPI-Host": """tank01-mlb-live-in-game-real-time-statistics
+                        .p.rapidapi.com"""
 }
 
 response = requests.get(url, headers=headers, params=querystring)
--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/soccer')
 def soccer():
     return render_template('soccer.html')
+
 
 @app.route('/baseball')
 def baseball():
