4d43540312ead6e1d172c3216a59e21fcf2c4249
LluisFabregatRiesco <luigi.fabregat@gmail.com>
2023-11-24 22:29:08 +0000

    Fixed mypy problems

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
+
+from typing import Any
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def home() -> Any:
     return render_template('home.html')
 
 
 @app.route('/soccer')
-def soccer():
+def soccer() -> Any:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
     return render_template('soccer.html')
 
 
 @app.route('/baseball')
-def baseball():
+def baseball() -> Any:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
     return render_template('baseball.html')
