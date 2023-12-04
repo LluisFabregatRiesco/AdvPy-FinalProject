391015cc8bc05034ce20615365b359f41175aa82
LluisFabregatRiesco <luigi.fabregat@gmail.com>
2023-12-04 19:18:39 +0000

    Updated templates

--- app/main.py
+++ app/main.py
@@ -5,9 +5,13 @@ This is the main file of the Flask application.
 from typing import Any
 import os
 from flask import Flask, render_template
-import db_api
+# from flask import request
+# from . import forms
+from . import db_api
 
 app = Flask(__name__)
+app.secret_key = 'b720367a8089e1e1b15ba89252ce07'
+app.secret_key += 'b8393320dad043d609c01366a6cfe7946e'
 
 
 @app.route('/')
@@ -24,55 +28,68 @@ def home() -> Any:
     return render_template('home.html', **context)
 
 
-@app.route('/soccer')
-def soccer() -> Any:
-    """Soccer page of the application.
+@app.route('/baseball')
+def baseball() -> Any:
+    """Recipes page of the application.
 
     Returns:
         str: HTML page using Jinja2 template.
     """
+    documents = db_api.find_all({})
+
+    context = {
+        'title': 'Baseball',
+        'recipes': documents['documents']
+    }
+    return render_template('baseball.html', **context)
+
+
+@app.route('/soccer')
+def soccer() -> Any:
+    """Recipes page of the application.
+
+    Returns:
+        str: HTML page using Jinja2 template.
+    """
+    documents = db_api.find_all({})
+
     context = {
         'title': 'Soccer',
+        'recipes': documents['documents']
     }
     return render_template('soccer.html', **context)
 
 
 @app.route('/mma')
 def mma() -> Any:
-    """MMA page of the application.
+    """Recipes page of the application.
 
     Returns:
         str: HTML page using Jinja2 template.
     """
+    documents = db_api.find_all({})
+
     context = {
         'title': 'MMA',
+        'recipes': documents['documents']
     }
     return render_template('mma.html', **context)
 
 
 @app.route('/basketball')
 def basketball() -> Any:
-    """Basketball page of the application.
+    """Recipes page of the application.
 
     Returns:
         str: HTML page using Jinja2 template.
     """
+    documents = db_api.find_all({})
+
     context = {
         'title': 'Basketball',
+        'recipes': documents['documents']
     }
-    return render_template('Basketball.html', **context)
-
-
-@app.route('/baseball')
-def baseball() -> Any:
-
-    db_api.insert_one({"Test": 5, "Test Again": 8})
-
-    res = db_api.find_all({})
-
-    print(res)
-
-    return render_template('baseball.html')
+    return render_template('basketball.html', **context)
 
 
 if __name__ == "__main__":
